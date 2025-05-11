3b607c03ae768b9b92ffa352bb1216a6fda7eefc
Kent <Kent>
2025-05-11 12:26:53 -0400

    Added main game loop

--- main.py
+++ main.py
@@ -14,4 +14,18 @@ class FarmplotTile:
 class Farmplot:
     pass
 
+# general setup
+pygame.init()
+pygame.display.set_caption("Space Farm")
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+main_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #Handles "X"ing out of the game
+            running = False
+
+pygame.quit() 
+
 
