0cf69b11b4f0ec85da79d9162507cdd290b70d5c
Kent <Kent>
2025-05-11 13:38:53 -0400

    Fleshed out player class, added movement handling centered player sprite, added additional bg image

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ class Background(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surface
         self.rect = self.image.get_frect(topleft = position) 
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 600
 
-class Player:
-    pass
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, surface, position, groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft = position)
+
+    def update(self, dt):
+        pass
 
 class Plant:
     pass
@@ -30,12 +41,18 @@ clock = pygame.time.Clock() #Creates the main clock object
 
 # Imports
 island_background_surf = pygame.image.load(join("img", "MainMap.png")).convert()
+farmPlot1_start_surf = pygame.image.load(join("img", "fp1-Start.png")).convert()
+playerDown = pygame.image.load(join("img", "playerDown.png")).convert_alpha()
+
 
 # Sprites
 all_sprites = pygame.sprite.Group() #Group for all sprites
+moving_background_sprites = pygame.sprite.Group() # All sprites that move with the background image
 
-island_background_sprite = Background(island_background_surf, (-5500,-4500), all_sprites)
+island_background_sprite = Background(island_background_surf, (-5500,-4500), (all_sprites, moving_background_sprites))
+fp1_start_sprite = Background(farmPlot1_start_surf, (0, 0), (all_sprites, moving_background_sprites))
 
+player = Player(playerDown, (WINDOW_WIDTH/2,WINDOW_HEIGHT/2 - 32), all_sprites)
 
 
 while running:
@@ -46,7 +63,19 @@ while running:
         if event.type == pygame.QUIT: #Handles "X"ing out of the game
             running = False
 
+    # Movement
+    keys = pygame.key.get_pressed()
+    for sprite in moving_background_sprites:
+        sprite.direction.x = (int(keys[pygame.K_LEFT]) or int(keys[pygame.K_a])) - (int(keys[pygame.K_RIGHT]) or int(keys[pygame.K_d]))
+        sprite.direction.y = (int(keys[pygame.K_UP]) or int(keys[pygame.K_w])) - (int(keys[pygame.K_DOWN]) or int(keys[pygame.K_s]))
+        
+
+    # Update
+    all_sprites.update(dt)        
+
+    # Draw
     all_sprites.draw(main_display)
+
     pygame.display.update()
 
 
